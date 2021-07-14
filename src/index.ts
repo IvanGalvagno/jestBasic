856fdef2cc1815d8b70fcc7203c8d4d1313de9ee
igsrc <ivan.galvagno@gmail.com>
2021-07-13 21:26:00 -0300

    Creating API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import * as express from 'express';
 
+
+const PORT = 3333;
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.listen(PORT, () =>{
+    console.log(`Api started at PORT ${PORT}`)
+});
 
 
 
