1066cc1a870b6860ec1432070c26a517af4337bf
igsrc <ivan.galvagno@gmail.com>
2021-07-14 12:38:17 -0300

    import

--- tests/validatePassword.ts
+++
@@ -1,7 +0,0 @@
-export default function validatePassword(password:string) {
-
-   let validLength = password.length >= 8;
-   let hasLetters = /[a-zA-Z]/g.test(password);
-   let hasNumber = /[0-9]/g.test(password);
-   return hasNumber && hasLetters && validLength
-}
