34de3f5a36ae882c5f9e5d2b97782972c6925994
igsrc <ivan.galvagno@gmail.com>
2021-07-13 21:27:05 -0300

    Importing routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import * as express from 'express';
+import routes from "./routes";
 
 
 const PORT = 3333;
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(routes)
 
 app.listen(PORT, () =>{
     console.log(`Api started at PORT ${PORT}`)
